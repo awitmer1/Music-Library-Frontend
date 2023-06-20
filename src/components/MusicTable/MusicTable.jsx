72f1d31e85ac5b9e33aee0e716181ad455f69f16
Andrew Witmer <witmer.a@gmail.com>
2023-06-20 09:03:12 -0700

    Added song table component - awaiting data response

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function MusicTable() {
+const MusicTable = (props) => {
   return (
-    <div className="music-table">
-      <p>MusicTable</p>
-      </div>
-  )
-}
+    <div className="table-container">
+      <h3>MusicTable</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.songs.map((entry, index) => {
+            return (
+              <tr key={index}>
+                <td>{props.songs.title}</td>
+                <td>{props.songs.artist}</td>
+                <td>{props.songs.album}</td>
+                <td>{props.songs.release_date}</td>
+                <td>{props.songs.genre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default MusicTable
+export default MusicTable;
